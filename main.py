93994cd747bf7ddbf46540c26bef44099a34338e
Bruno Guimarães <brunoguima13@gmail.com>
2023-08-29 19:19:46 -0300

    Adicionado entrada e nova forma de construir a matriz

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
-n = input()
 matriz = []
 qtd_preto = 0
 alternativas = ["A", "B", "C", "D", "E"]
+entrada = """
+3
+0 255 255 255 255
+255 255 255 255 0
+255 255 127 255 255
+4
+200 200 200 0 200
+200 1 200 200 1
+1 2 3 4 5
+255 5 200 130 205
+0
+"""
 
-for i in range(int(n)):
-    linhas = input().split()
-    linhas = list(map(int, linhas))
-    matriz.append(linhas)
+linhas_entrada = entrada.strip().split('\n')
+i = 0
+while i < len(linhas_entrada):
+    n = int(linhas_entrada[i])
+    i += 1
+    for _ in range(n):
+        linha = list(map(int, linhas_entrada[i].split()))
+        matriz.append(linha)
+        i += 1
 
-while qtd_preto < 1 and int(n) > 0:
+# for i in range(int(n)):
+#     linhas = input().split()
+#     linhas = list(map(int, linhas))
+#     matriz.append(linhas)
+
+
+while qtd_preto < 1:
     for linhas in matriz:
         qtd_preto = 0
         for i, numero in enumerate(linhas):
             if numero <= 127:
                 qtd_preto += 1
                 resultado_aluno = alternativas[i]
-                pass
             if qtd_preto > 1:
                 resultado_aluno = "*"
         print(resultado_aluno)
