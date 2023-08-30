5e68184ed45f63f8c0c70089b995a8d7913cc9e8
Bruno Guimarães <brunoguima13@gmail.com>
2023-08-29 23:13:44 -0300

    Adicionando nova forma de validação e documentando funções

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
-alternativas = ["A", "B", "C", "D", "E"]
-
-
-def checar_resposta(resposta):
-    qtd_preto = 0
-    resultado_aluno = ""
-    for i, numero in enumerate(resposta):
-        if numero <= 127:
-            qtd_preto += 1
-            resultado_aluno = alternativas[i]
-        if qtd_preto > 1:
-            resultado_aluno = "*"
-            return resultado_aluno
-    return resultado_aluno
-
-
-def procurar_resposta(qtd_linhas):
+# Função responsável por montar e processar as respostas dos alunos.
+def resolver_respostas(qtd_linhas):
     for _ in range(int(qtd_linhas)):
         linhas = input().split()
         linhas = list(map(int, linhas))
-        print(checar_resposta(linhas))
+        print(validar_respostas(linhas))
     return
 
 
+# Função responsável por fazer a validação das respostas e retornar a alternativa marcada.
+def validar_respostas(respostas):
+    index_alternativa = [i for i, valor in enumerate(respostas) if valor <= 127]
+    if len(index_alternativa) == 1:
+        return chr(index_alternativa[0] + ord('A'))
+    else:
+        return "*"
+
+
+# Loop de inicialização forçada com condição para finalizar execução quando for passado 0 ao input.
 while True:
     qtd_linhas = input()
     if int(qtd_linhas) == 0:
         break
-    procurar_resposta(qtd_linhas)
-
-
-
-
+    resolver_respostas(qtd_linhas)
